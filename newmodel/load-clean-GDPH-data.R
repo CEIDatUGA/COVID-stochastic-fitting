8addf2f9c72b6968c1c1ef044306016998a5bafa
Andreas Handel <andreas.handel@gmail.com>
2020-04-05 19:33:39 -0400

    added covidtracker data stream

--- newmodel/load-clean-GDPH-data.R
+++ newmodel/load-clean-GDPH-data.R
@@ -1,5 +1,5 @@
 # load-clean-data.R
-# load and clean/process data so it is ready for fitting
+# load and clean/process Ga Dept of PH data so it is ready for fitting
 
 # Load libraries ----------------------------------------------------------
 
@@ -8,7 +8,7 @@ library(here) #to simplify loading/saving into different folders
 
 
 ## Load data
-data <- read.csv(here('GA_daily_status_report_GDPH.csv'))
+data <- read.csv(here('/data/GA_daily_status_report_GDPH.csv'))
 data$date <- as.Date(data$date, format='%m/%d/%y')
 
 # process/clean data
@@ -29,13 +29,13 @@ future <- data.frame(time = max(covid_ga_data$time):(max(covid_ga_data$time)+31)
 # Turn data into new case reports each day, to match the simulation model
 # and to follow the advice of King et al. 2015 PRSB (fit to incidence reports
 # not cumulative case reports).
-covid_ga_data$cases <- c(1, diff(covid_ga_data$cases))
+covid_ga_data$Daily_Cases <- c(1, diff(covid_ga_data$cases))
 # covid_ga_data$cases <- covid_ga_data$cases + 1 # Add one to avoid 0s for fits
 covid_ga_data$hosps <- NA  # placeholder until data stream set up
 covid_ga_data$deaths <- NA  # placeholder until data stream set up
 
 # Save cleaned data
-filename = here('outputnew/clean-data.RDS')
+filename = here('data/clean-GDPH-data.RDS')
 
 saveRDS(covid_ga_data, filename)
 
