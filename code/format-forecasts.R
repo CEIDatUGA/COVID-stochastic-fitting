b0d0e7580457feb196c74a773ffd27be5a4481ad
Andrew Tredennick <atredennick@west-inc.com>
2020-04-09 15:30:40 -0600

    Adds script to collate and save forecasts

--- code/format-forecasts.R
+++ code/format-forecasts.R
@@ -1,79 +1,148 @@
 # format-forecasts.R
+# This script formats forecasts made by the run-particle-mcmc.R script.
+# Forecasts are formatted in two ways:
+#   1. A data frame of new hospitalizations indexed by simulation rep
+#      and date.
+#   2. A list of data frames, where each data frame is the output from a 
+#      single simulation rep for all variables over the simulate dates.
+
+
+# Clear the decks ---------------------------------------------------------
 
 rm(list = ls(all.names = TRUE))
+
+SAVE_RESULTS <- TRUE
+MAKE_PLOTS <- FALSE
+
+
+# Load libraries ----------------------------------------------------------
 
 library(tidyverse)
 library(here)
 library(pomp)
 
+
+# Load the pomp model and the pmcmc output list ---------------------------
+
 pomp_model <- readRDS(here("output/pomp-model.RDS"))
 mcmcs <- readRDS(here("output/pmcmc-output.RDS"))
 
-start_date <- as.Date("2020-03-01")
-end_date <- start_date + max(time(mcmcs[[1]]))
-dates <- seq.Date(start_date, end_date, "days") 
-dates_df <- data.frame(time = c(1:length(dates)), Date = dates)
 
-dat <- t(pomp_model@data) %>% 
+# Define a few global settings regarding dates and observations -----------
+
+start_date <- as.Date("2020-03-01")  # start of simulation/fitting
+end_date <- start_date + max(time(mcmcs[[1]]))  # end of forecast
+dates <- seq.Date(start_date, end_date, "days")   # all the dates
+
+# Make a data frame that maps 'time' to 'Date'
+dates_df <- data.frame(time = c(1:length(dates)), 
+                       Date = dates)
+
+# Read in the observation data associated with pomp model object
+# and reformat into a long data frame.
+dat <- t(pomp_model@data) %>%  
   as.data.frame() %>%
   mutate(time = 1:n()) %>%
-  left_join(dates_df, by = "time")
-
+  left_join(dates_df, by = "time")  # add in dates
 
 
 # Collate for the forecast competition ------------------------------------
 
-hosps_forecasts <- tibble()
+hosps_forecasts <- tibble()  # empty storage object
 
+# Loop over the pMCMC chains and extract new hospitalization estimates
+# and forecasts.
 for(i in 1:length(mcmcs)) {
-  tmp <- mcmcs[[i]]
-  tmp <- tmp@filter.traj
-  tmp <- t(tmp["H_new", 1001:2000, ]) %>%
+  tmp <- mcmcs[[i]]  # the pmcmc object
+  tmp <- tmp@filter.traj  # the filtered trajectories for each MCMC iteration
+  
+  # Reshape the large matrix into a long dataframe indexed by
+  # time, chain, and iteration
+  tmp <- t(tmp["H_new", 1001:2000, ]) %>%  # only use 1000 final iterations
     as.data.frame() %>%
     gather(key = "iter", value = "H_new") %>%
     group_by(iter) %>%
     mutate(time = 1:n()) %>%
     ungroup() %>%
-    mutate(chain = i,
+    mutate(chain = i,  # add chain id
            iter = as.numeric(iter)) %>%
     left_join(dates_df, by = "time") %>%
-    dplyr::select(chain, iter, Date, H_new)
+    dplyr::select(chain, iter, Date, H_new)  # just keep relevant variables
   
+  # Bind the rows for storage
   hosps_forecasts <- bind_rows(hosps_forecasts, tmp)
 }
 
+# Final formatting for the forecasting team
 hosps_forecasts <- hosps_forecasts %>%
   mutate(Rep = as.numeric(as.factor(paste(chain, iter)))) %>%
   dplyr::select(Rep, Date, H_new)
 
-hosps_summary <- hosps_forecasts %>%
-  group_by(Date) %>%
-  summarise(lower = quantile(H_new, 0.025),
-            med = median(H_new),
-            upper = quantile(H_new, 0.975)) %>%
-  ungroup() %>%
-  mutate(Period = ifelse(Date > Sys.Date(), "Forecast", "Calibration"))
+# Save the object for forecasting team to format for submission
+if(SAVE_RESULTS) {
+  filename <- paste0("output/hosps-forecasts-", Sys.Date(), ".RDS")
+  saveRDS(object = as.data.frame(hosps_forecasts),
+          file = filename)
+}
 
-p1 <- ggplot(hosps_summary %>% filter(Date < "2020-04-24"), 
-       aes(x = Date, y = med)) +
-  geom_ribbon(aes(ymin = lower, ymax = upper, fill = Period),
-              alpha = 0.2, color = NA) +
-  geom_line(aes(color = Period)) +
-  geom_point(data = dat, aes(y = hosps)) +
-  labs(y = "Number of new hospitalizations") +
-  ggtitle("2-week forecast")
 
-p2 <- ggplot(hosps_summary, aes(x = Date, y = med)) +
-  geom_ribbon(aes(ymin = lower, ymax = upper, fill = Period),
-              alpha = 0.2, color = NA) +
-  geom_line(aes(color = Period)) +
-  geom_point(data = dat, aes(y = hosps)) +
-  labs(y = "Number of new hospitalizations") +
-  ggtitle("7-week forecast")
+# Make some forecast summaries and figures --------------------------------
 
-cowplot::plot_grid(p1, p2, ncol = 2)
+if(MAKE_PLOTS) {
+  hosps_summary <- hosps_forecasts %>%
+    group_by(Date) %>%
+    summarise(lower = quantile(H_new, 0.025),
+              med = median(H_new),
+              upper = quantile(H_new, 0.975)) %>%
+    ungroup() %>%
+    mutate(Period = ifelse(Date > Sys.Date(), "Forecast", "Calibration"))
+  
+  two_week_days <- 16  # 14 days plus 2 to account for lag from Thurs. to the Sat.
+  p1 <- ggplot(hosps_summary %>% filter(Date < (Sys.Date() + 16)), 
+               aes(x = Date, y = med)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Period),
+                alpha = 0.2, color = NA) +
+    geom_line(aes(color = Period)) +
+    geom_point(data = dat, aes(y = hosps)) +
+    labs(y = "Number of new hospitalizations") +
+    ggtitle("2-week forecast")
+  
+  p2 <- ggplot(hosps_summary, aes(x = Date, y = med)) +
+    geom_ribbon(aes(ymin = lower, ymax = upper, fill = Period),
+                alpha = 0.2, color = NA) +
+    geom_line(aes(color = Period)) +
+    geom_point(data = dat, aes(y = hosps)) +
+    labs(y = "Number of new hospitalizations") +
+    ggtitle("7-week forecast")
+  
+  cowplot::plot_grid(p1, p2, ncol = 2)
+}
 
-# Save the forecasts
-saveRDS(object = as.data.frame(hosps_forecasts), 
-        file = here("output/hosps-forecasts-20200409.RDS"))
+
+# Format full forecast results --------------------------------------------
+
+all_forecasts <- list()
+count <- 1  # list index counter
+for(i in 1:length(mcmcs)) {
+  tmp <- mcmcs[[i]]  # the pmcmc object
+  tmp <- tmp@filter.traj  # the filtered trajectories for each MCMC iteration
+  tmp <- tmp[ , 1001:2000, ]
+  
+  for(j in 1:dim(tmp)[2]) {
+    all_forecasts[[count]] <- t(tmp[ , j , ]) %>% 
+      as.data.frame() %>%
+      mutate(time = 1:n()) %>%
+      left_join(dates_df, by = "time") 
+    
+    count <- count + 1  # advance list index counter
+  }  # end iteration loop
+  
+}  # end chain loop
+
+# Save the results
+if(SAVE_RESULTS) {
+  filename <- paste0("output/all-states-forecasts-", Sys.Date(), ".RDS")
+  saveRDS(object = as.data.frame(hosps_forecasts),
+          file = filename)
+}
 
