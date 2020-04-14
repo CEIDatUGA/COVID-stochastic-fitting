276f5c7665862cb6236c90db04b23527d3e0dbeb
Andreas Handel <andreas.handel@gmail.com>
2020-04-14 17:20:56 -0400

    code fixes that got lost in the last push

--- code/set-priors.R
+++ code/set-priors.R
@@ -6,22 +6,14 @@ rm(list = ls(all.names = TRUE))
 #values for priors are explained in a separate spreadsheet:
 #https://docs.google.com/spreadsheets/d/10K_3bPck0GOCAfuUQtyZ-KEmwf6FQE3CcpSe36HtL4c/edit#gid=478517635
 
-#code below implements them so they can be used by pomp
-
+# code below implements them so they can be used by pomp
 # mean/central values for parameters, as specified by spreadsheet
 # for meaning of parameters, see spreadhseet
-
 
 #load values for model parameters and initial conditions
 #use those as mean/central value for prior definition
 filename = here('output/var-par-definitions.RDS')
 x <- readRDS(filename) #for ease of notation, assign parameter values to x
-
-# the idea is to use the point estimates/central values above
-#and add a distribution with some spread to define priors
-#this should be done only for parameters that are estimated
-
-##Since we don't know how to pass variables into this Csnippet, we create it as a string
 
 param_sds <- 8
 prior_par_list = list()
@@ -35,7 +27,7 @@ for (nn in 1:length(x$params_to_estimate))
   prior_par_list[[nn]] = paste0("dnorm(", x$params_to_estimate[nn],", ",as.numeric(x$allparvals[x$params_to_estimate[nn]]),", ",param_sds, ", 1)")
 }
 
-for (nn in 1:length(x$inivals_to_estimate))
+for (nn in 1:length(x$inivals_to_estimate)) #need different sds, so extra loop
 {
   prior_ini_list[[nn]] = paste0("dnorm(", x$inivals_to_estimate[nn],", ",x$allparvals[x$inivals_to_estimate[nn]],", ",ini_sds, ", 1)")
 }
@@ -44,7 +36,6 @@ s1 = paste0(prior_par_list, collapse = " + ")
 s2 = paste0(prior_ini_list, collapse = " + ")
 prior_dens_text = paste0("lik = ",s1," + ",s2, " ; \n " ,
                          "if (!give_log) lik = exp(lik);" )
-
 
 prior_dens <- pomp::Csnippet(
   prior_dens_text
