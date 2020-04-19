c869df53e759a254f39b3ede4d8cb62294eb3ebb
Andreas Handel <andreas.handel@gmail.com>
2020-04-19 09:44:25 -0400

    moved code into sub-folders

--- code/00-MASTER-RUN-ANALYSIS.R
+++ code/00-MASTER-RUN-ANALYSIS.R
@@ -26,12 +26,12 @@ library(here)
 # this will then be automatically passed through all the fitting scripts
 # assigns values to variables and initial conditions
 # results are written into RDS file and loaded by later scripts
-source(here("code/set-pars-and-vars.R"))
+source(here("code/model-setup/set-pars-and-vars.R"))
 
 # Run script to set priors
 # needs results from set-pars-and-vars.R 
 # results are written into RDS file and loaded by later scripts
-# source(here("code/set-priors.R"))
+# source(here("code/model-setup/set-priors.R"))
 
 # Run data cleaning script. Then load cleaned data ready for pomp --------------------------------------------
 # results saved to data folder with time stamp in name
@@ -43,18 +43,18 @@ source(here("code/data-processing/load-clean-CT-data.R"))
 # results will be loaded by later files
 thefiles <- list.files(path = here("data/"), pattern = "ga_state_raw")
 if(length(thefiles) != 0) {
-  source(here("code/model-beta-reduction.R"))
+  source(here("code/model-setup/model-beta-reduction.R"))
 }
 
 # Make a pomp model 
 # loads all the previously generated RDS files and generates a pomp model that's ready for fitting
 # the resulting pomp object is saved as RDS file into the output folder
-source(here("code/make-pomp-model.R"))
+source(here("code/model-setup/make-pomp-model.R"))
 
 
 # Run the mif fitting routine -----------------------------------------------------
 # loads the previously generated pomp model 
-source(here("code/run-mif.R"))
+source(here("code/model-fitting/run-mif.R"))
 
 # Does post processing and exploration on the best fit mif results -----------------------------------------------------
 source(here("code/result-exploration/explore-mif-results.R"))
@@ -73,7 +73,7 @@ source(here("code/result-exploration/explore-simulation-results.R"))
 
 
 # Run the ABC-MCMC --------------------------------------------------------
-# source(here("code/run-abc.R"))
+# source(here("code/model-fitting/run-abc.R"))
 
 
 
