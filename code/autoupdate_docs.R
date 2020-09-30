473b8705c0da103b416b484215329664352469cb
Eric Marty <allopole@noreply.github.com>
2020-09-30 10:15:48 -0400

    automated render

---
+++ code/autoupdate_docs.R
@@ -0,0 +1,12 @@
+# script to automatically render files from crontab (MacOS)
+library(rmarkdown)
+library(here)
+
+# pandoc location must be hard coded. Find with Sys.getenv("RSTUDIO_PANDOC")
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+
+path <- paste0(here::here(),"/docs/")
+rmarkdown::render(paste0(path,'overview.Rmd'),output_file=paste0(path,'overview.html'))
+# setwd("docs")
+rmarkdown::render(paste0(path,'model-details.Rmd'),output_file=paste0(path,'model-details.html'))
+rmarkdown::render(paste0(path,'states-model.Rmd'),output_file=paste0(path,'states-model.html'))
