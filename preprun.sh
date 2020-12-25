91816565c1d329638ca8ee5d0093daf6fbd7de24
Eric Marty <allopole@noreply.github.com>
2020-12-25 08:47:27 -0500

    automated update of docs and shinydata

---
+++ preprun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run this script from the command line after the mif-array.sh has run on sapelo2
+# first cd to top level project directory, then:
+# >bash preprun.sh
+cp -r ~/COVID-stochastic-fitting/data/ ~/cov/
+cp -r ~/COVID-stochastic-fitting/code/ ~/cov/
---
+++ saveoutput.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Run this script from the command line after the mif-array.sh has run on sapelo2
+# first cd to top level project directory, then:
+# >bash saveoutput.sh
+cd ~/COVID-stochastic-fitting  
+git pull  
+cp -r ~/cov/output/current/ ~/COVID-stochastic-fitting/output/
+cd ~/cov/output
+LATESTDIR=$(ls -td 202* | head -1)
+LATESTDIRFULL="full$(echo $LATESTDIR | sed -e 's/-//g')"
+cp -r $LATESTDIR ~/COVID-stochastic-fitting/output/
+cd /work/covid19lab/full
+mkdir $LATESTDIRFULL
+cp ~/cov/output/*.rds $LATESTDIRFULL
