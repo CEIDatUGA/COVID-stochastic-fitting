3d8d8a66b000b60f0fb657e3cb04bc2f57785109
Eric Marty <allopole@noreply.github.com>
2021-02-01 14:05:05 -0500

    manual update of docs and shinydata

--- code/plotting/render_diagnostic_plots.R
+++ code/plotting/render_diagnostic_plots.R
@@ -6,8 +6,8 @@
 # states <- c("Indiana", "Maryland", "New Jersey", "Ohio", "Washington")
 # states <- c("Indiana", "Massachusetts", "Michigan", "New Jersey", "Ohio", "Washington")
 # states <- c("Indiana", "Massachusetts", "New Jersey", "New York", "Ohio", "Washington")
-states <- c("Washington")
-startdate <- "2020-11-01" # Y-m-d
+states <- c("Maryland")
+startdate <- "2021-01-01" # Y-m-d
 enddate <- format(Sys.time(), '%Y-%m-%d')
 
 # Don't alter the code below ------------------------------------------------------------------
