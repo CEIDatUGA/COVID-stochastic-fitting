454891b768e8692bfe428db7e9f481bb33d6d8a3
Eric Marty <allopole@noreply.github.com>
2021-03-05 08:49:43 -0500

    automated update of docs and shinydata

--- code/result-exploration/getReff.R
+++ code/result-exploration/getReff.R
@@ -4,47 +4,47 @@
 # dates <- seq(min(dates),max(dates),1)
 # t <- seq_along(dates)
 
-get_q <- function(t, params) {
+  get_q <- function(t, params) {
+    
+    # detection probability # check against pomp code
+    q_min <- params$base_detect_frac
+    q_max <- params$max_detect_frac
+    q_n <- params$detect_rampup # Hill coefficient
+    q_th <- params$t_half_detect 
+    # as defined in states-model.rmd:
+    q <- scales::rescale(
+      t^q_n / (q_th^q_n + t^q_n), 
+      from = c(0,1), 
+      to = c(q_min, q_max)
+    )
+    # as defined in makepompmodel:
+    # 1/(1+exp(max_detect_par)) * exp(log_detect_inc_rate)^t / (exp(log_detect_inc_rate)^exp(log_half_detect) + exp(log_detect_inc_rate)^t) + base_detect_frac
+    # q <- 1/(1 + q_max) * q_n ^ t / (q_n ^ q_th + q_n ^ t) + q_min # does not jive with q caluclation above
+    
+    return(q)
+  }
   
-  # detection probability # check against pomp code
-  q_min <- params$base_detect_frac
-  q_max <- params$max_detect_frac
-  q_n <- params$detect_rampup # Hill coefficient
-  q_th <- params$t_half_detect 
-  # as defined in states-model.rmd:
-  q <- scales::rescale(
-    t^q_n / (q_th^q_n + t^q_n), 
-    from = c(0,1), 
-    to = c(q_min, q_max)
-  )
-  # as defined in makepompmodel:
-  # 1/(1+exp(max_detect_par)) * exp(log_detect_inc_rate)^t / (exp(log_detect_inc_rate)^exp(log_half_detect) + exp(log_detect_inc_rate)^t) + base_detect_frac
-  # q <- 1/(1 + q_max) * q_n ^ t / (q_n ^ q_th + q_n ^ t) + q_min # does not jive with q caluclation above
+  # This fuction takes the dates and the dataframe of parameters in natural units, 
+  # and returns a vector for diagnosis rate (s).
   
-  return(q)
-}
-
-# This fuction takes the dates and the dataframe of parameters in natural units, 
-# and returns a vector for diagnosis rate (s).
-
-get_s <- function(t, params) {
-  
-  # diagnosis rate 
-  s_max <- params$max_diag_factor
-  s_n <- params$diag_rampup # Hill coefficient
-  s_th <- params$t_half_diag 
-  # s <- scales::rescale(
-  #   t^s_n / (s_th^s_n + t^s_n),
-  #   from = c(0,1),
-  #   to = c(s_min, s_max)
-  #   )
-  
-  # as defined in makepompmodel:
-  # 1 + exp(log_max_diag) * exp(log_diag_inc_rate)^t /  ( exp(log_diag_inc_rate)^exp(log_half_diag) +   exp(log_diag_inc_rate)^t    )
-  s <- 1 + s_max * s_n^t / ( s_n ^ s_th + s_n ^ t )
-  
-  return(s)
-}
+  get_s <- function(t, params) {
+    
+    # diagnosis rate 
+    s_max <- params$max_diag_factor
+    s_n <- params$diag_rampup # Hill coefficient
+    s_th <- params$t_half_diag 
+    # s <- scales::rescale(
+    #   t^s_n / (s_th^s_n + t^s_n),
+    #   from = c(0,1),
+    #   to = c(s_min, s_max)
+    #   )
+    
+    # as defined in makepompmodel:
+    # 1 + exp(log_max_diag) * exp(log_diag_inc_rate)^t /  ( exp(log_diag_inc_rate)^exp(log_half_diag) +   exp(log_diag_inc_rate)^t    )
+    s <- 1 + s_max * s_n^t / ( s_n ^ s_th + s_n ^ t )
+    
+    return(s)
+  }
   
 # This fuction takes the dates, number of susceptibles, population, omega, 
 # and the dataframe of parameters in natural units, and returns a vector for R effective.
