245b836691757cae302e00303bdb555d431cd19d
Eric Marty <allopole@noreply.github.com>
2021-04-10 08:13:08 -0400

    update diagnostic plots

--- code/plotting/render_diagnostic_plots.R
+++ code/plotting/render_diagnostic_plots.R
@@ -6,8 +6,8 @@
 # states <- c("Indiana", "Maryland", "New Jersey", "Ohio", "Washington")
 # states <- c("Indiana", "Massachusetts", "Michigan", "New Jersey", "Ohio", "Washington")
 # states <- c("Indiana", "Massachusetts", "New Jersey", "New York", "Ohio", "Washington")
-states <- c("Washington")
-startdate <- "2021-02-15" # Y-m-d
+states <- c( "Arizona","California","Illinois","Massachusetts")
+startdate <- "2021-03-01" # Y-m-d
 enddate <- format(Sys.time(), '%Y-%m-%d')
 
 # Don't alter the code below ------------------------------------------------------------------
